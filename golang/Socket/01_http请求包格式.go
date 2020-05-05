e1a5d5c49b30af7e8667a2ec39fd608ac935f93e
hollson <hollson@qq.com>
2020-05-06 00:41:33 +0800

    Rebuilded at 2020年05月06日 00时41分33秒

--- golang/Socket/01_http请求包格式.go
+++ golang/Socket/01_http请求包格式.go
@@ -9,6 +9,7 @@ import (
 // 1.创建net.listen服务,network类型为tcp，端口为：8000
 // 2.根据创建的监听对象，创建conn链接对象
 // 3.基于Conn对象，读取字节流数据，使用字节数据接收，即n,err:=conn.Read(buf)
+// 4.buf是一个字节切片，创建方式如：buf:=make(byte[],1<<20),即1M大小
 func main() {
 	// 创建listen-tcp监听服务，使用:8080端口
 	listener, err := net.Listen("tcp", ":8000")
