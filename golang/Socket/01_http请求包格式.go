a8caa9787f803b78da93007fe90bd437f6a6dfdb
hollson <hollson@qq.com>
2020-05-06 00:38:01 +0800

    Rebuilded at 2020年05月06日 00时38分01秒

--- golang/Socket/01_http请求包格式.go
+++ golang/Socket/01_http请求包格式.go
@@ -5,33 +5,31 @@ import (
 	"net"
 )
 
+// 肌肉记忆
+// 1.创建net.listen服务,network类型为tcp，端口为：8000
+// 2.根据创建的监听对象，创建conn链接对象
+// 3.基于Conn对象，读取字节流数据，使用字节数据接收，即n,err:=conn.Read(buf)
 func main() {
-	//监听
-	listener, err := net.Listen( "tcp", ":8000")
+	// 创建listen-tcp监听服务，使用:8080端口
+	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
-		fmt.Println("Listen err = ", err)
-		return
+		panic(err)
 	}
-
 	defer listener.Close()
 
-	//阻塞等待用户的连接
-	conn, err1 := listener.Accept()
-	if err1 != nil {
-		fmt.Println("Accept err1 = ", err1)
-		return
+	//基于当前监听服务，创建链接对象
+	conn, err := listener.Accept()
+	if err != nil {
+		panic(err)
 	}
-
 	defer conn.Close()
 
-	//接收客户端的数据
-	buf := make([]byte, 1024*4)
-	n, err2 := conn.Read(buf)
-	if n == 0 {
-		fmt.Println("Read err = ", err2)
-		return
+	//基于当前链接，接收数据
+	buf := make([]byte, 1<<20) //1M
+	n,err:= conn.Read(buf)
+	if err!=nil{
+		panic(err)
 	}
 
-	fmt.Printf("#%v#", string(buf[:n]))
-
+	fmt.Println(string(buf[:n]))
 }
